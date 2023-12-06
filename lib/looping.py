c38acc20b0668cf1eedbe7cf2930b37ad24de197
root <+>
2023-12-06 18:38:14 +0300

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,34 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+      print(count)
+      count -= 1
+      if count == 0:
+         print("Happy New Year!")
+      
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = []
+    for i in int_list:
+       squared_value = i ** 2
+       squared_list.append(squared_value)
+    return squared_list
+print(square_integers([1, 2, 3]))
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for number in range(1, 101):
+       if number % 3 == 0 and number % 5 == 0:
+          print("FizzBuzz")
+       elif number % 3 == 0:
+          print("Fizz")
+       elif number % 5 == 0:
+          print("Buzz")
+       else:
+          print(number)
+fizzbuzz()
